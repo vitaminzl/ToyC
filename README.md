be8327459218b02cd8d674c06dc78146c74d6a5f
ZL <ginrut@foxmail.com>
2022-05-14 15:17:11 +0800

    configure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Language-Long
+# Long语言
+本项目用于学习编译原理。  
+将参照龙书版本的《编译原理》  
+https://www.zhihu.com/question/36756224
+
+## 编译器的结构
+一个编译器需要什么？
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+    cout << "oh" << endl;
+    cout << "oh" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << i << endl;
+    }
+    system("pause");
+    return 0;
+}
