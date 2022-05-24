55902c5dffeb877674dfdea0ab03d51df7574ba6
ZL <ginrut@foxmail.com>
2022-05-24 19:27:02 +0800

    add test

---
+++ src/Test/Test.h
@@ -0,0 +1,14 @@
+#ifndef TEST
+#define TEST
+#include "../Inter/Inter.h"
+#include "../Lexer/Lexer.h"
+#include "../Parser/Parser.h"
+
+class Test{
+public:
+    void testScan();
+    void testSymbol();
+};
+
+
+#endif
